46e053d727efdb9f8bbdcf70d360f4996b30616f
Sanskriti Lamsal <sanskritilamsal@hst000148073um.wifi02.olemiss.edu>
2019-01-26 12:20:13 -0600

    Button and typing animation added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-
+import Typing from 'react-typing-animation';
 import './App.css';
-
+import maincontent from './pages/maincontent';
 class App extends Component {
   render() {
     return (
@@ -10,12 +10,23 @@ class App extends Component {
           <img className="wave" src ={require("./img/wavess.png")} />
           <img className = "circularlogo" src={require("./img/spiral.png")} />
           <img className="text" src ={require("./img/text.png")} />
-          
+          <p className="typing">{AnimatedTypingComponent()}</p>
+          <p>
+            <button type="button" className="launch btn btn-lg" onClick={<maincontent />}>
+              Launch Site
+            </button>
+          </p>
 
       </div>
     );
   }
 }
-
+const AnimatedTypingComponent =() => (
+  <Typing speed={60}>
+     
+     <span>Everything is figureoutable.</span>
+    
+  </Typing>
+);
 
 export default App;
---
+++ src/pages/maincontent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class maincontent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default maincontent
