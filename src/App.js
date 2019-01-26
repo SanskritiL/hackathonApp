af0f62caec23f5e1e0aac1abe69525e9d7baf382
Sanskriti Lamsal <sanskritilamsal@hst000148073um.wifi02.olemiss.edu>
2019-01-25 21:58:43 -0600

    Wave and Logo added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+          <img className="wave" src ={require("./img/wavess.png")} />
+          <img className = "circularlogo" src={require("./img/spiral.png")} />
+          <img className="text" src ={require("./img/text.png")} />
+          
+
       </div>
     );
   }
 }
+
 
 export default App;
